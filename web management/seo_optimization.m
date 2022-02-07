9d66cdacd8378055245ff36aedb88d68ab13c327
Huy Do <huydo91@outlook.com>
2022-02-07 14:41:14 +0700

    Create seo_optimization.m

---
+++ web management/seo_optimization.m
@@ -0,0 +1,5 @@
+## Một vài mục cần lưu khi đổi với 1 website đã public 
+
+1) SEO settings: seo tags, json-ld contents, well HTML structure 
+2) Monitor các traffic và tracking tương tác của người dùng: Google Tag Manager (config tracking), Google Analytic (nhận data và hiện các report)
+3) Quản lý SEO ở google: dùng Google console (https://search.google.com/search-console)
